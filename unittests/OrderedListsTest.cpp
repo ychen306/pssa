b773e09edb90f0d2a2f2deeaf9536954dd508e37
tom <ychen306@mit.edu>
2022-07-09 15:22:13 -0400

    fix test

--- unittests/OrderedListsTest.cpp
+++ unittests/OrderedListsTest.cpp
@@ -6,9 +6,9 @@ TEST(OrderredListTest, Test1) {
   List.push_back(1);
   List.push_back(3);
   List.insert(std::prev(List.end()), 2);
-  EXPECT_TRUE(List.comesBefore(1,2));
-  EXPECT_TRUE(List.comesBefore(2,3));
-  EXPECT_TRUE(List.comesBefore(1,3));
+  EXPECT_TRUE(List.comesBefore(1, 2));
+  EXPECT_TRUE(List.comesBefore(2, 3));
+  EXPECT_TRUE(List.comesBefore(1, 3));
 }
 
 TEST(OrderedTest, Test2) {
@@ -16,9 +16,9 @@ TEST(OrderedTest, Test2) {
   List.push_back(0);
   List.push_back(8);
   auto Last = std::prev(List.end());
-  for (unsigned i = 7; i > 0; i--)
+  for (unsigned i = 1; i < 8; i++)
     List.insert(Last, i);
   for (unsigned i = 0; i < 8; i++)
-    for (unsigned j = i+1; j < 8; j++)
-      EXPECT_TRUE(List.comesBefore(i, j));
+    for (unsigned j = i + 1; j < 8; j++)
+      EXPECT_TRUE(List.comesBefore(i, j)) << "; i = " << i << ", j = " << j;
 }
