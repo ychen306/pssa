be7ab6f06b0bc982f542cf0c5e3a88d6a414a23b
tom <ychen306@mit.edu>
2022-07-09 15:40:11 -0400

    tweak test

--- unittests/OrderedListsTest.cpp
+++ unittests/OrderedListsTest.cpp
@@ -1,11 +1,19 @@
 #include "utils/OrderedList.h"
 #include "gtest/gtest.h"
 
+namespace {
+template <typename T, typename Container>
+std::vector<T> to_vector(const Container &C) {
+  return std::vector<T>(C.begin(), C.end());
+}
+} // namespace
+
 TEST(OrderredListTest, Test1) {
   OrderedList<int> List;
   List.push_back(1);
   List.push_back(3);
   List.insert(std::prev(List.end()), 2);
+  EXPECT_EQ(to_vector<int>(List), std::vector<int>({1, 2, 3}));
   EXPECT_TRUE(List.comesBefore(1, 2));
   EXPECT_TRUE(List.comesBefore(2, 3));
   EXPECT_TRUE(List.comesBefore(1, 3));
@@ -19,6 +27,13 @@ TEST(OrderedTest, Test2) {
   auto Last = std::prev(List.end());
   for (unsigned i = 1; i < N; i++)
     List.insert(Last, i);
+
+  std::vector<int> Expected;
+  for (int i = 0; i < 500 + 1; i++)
+    Expected.push_back(i);
+
+  EXPECT_EQ(to_vector<int>(List), Expected);
+
   for (unsigned i = 0; i < N; i++)
     for (unsigned j = i + 1; j < N; j++)
       EXPECT_TRUE(List.comesBefore(i, j)) << "; i = " << i << ", j = " << j;
