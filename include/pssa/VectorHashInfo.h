0251e7c9ea31e9bd44e6a5172d62913c58e0bf69
tom <ychen306@mit.edu>
2022-07-27 14:14:55 -0400

    wip; tweak vectorhashinfo

--- include/pssa/VectorHashInfo.h
+++ include/pssa/VectorHashInfo.h
@@ -3,20 +3,22 @@
 
 #include "llvm/ADT/DenseMapInfo.h"
 #include "llvm/ADT/Hashing.h"
+#include "llvm/ADT/ArrayRef.h"
 
 template <typename VectorType> struct VectorHashInfo {
   using ElementType = typename VectorType::value_type;
+  using ArrayRef = typename llvm::ArrayRef<ElementType>;
   static VectorType getEmptyKey() {
     return {llvm::DenseMapInfo<ElementType>::getEmptyKey()};
   }
   static VectorType getTombstoneKey() {
     return {llvm::DenseMapInfo<ElementType>::getTombstoneKey()};
   }
-  static unsigned getHashValue(const VectorType &V) {
+  static unsigned getHashValue(ArrayRef V) {
     return static_cast<unsigned>(llvm::hash_combine_range(V.begin(), V.end()));
   }
-  static bool isEqual(const VectorType &LHS, const VectorType &RHS) {
-    return LHS == RHS;
+  static bool isEqual(ArrayRef LHS, ArrayRef RHS) {
+    return LHS.equals(RHS);
   }
 };
 
