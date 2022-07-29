01e7b6cd033a96c4d7dd7d5370a99abddc76f77d
tom <ychen306@mit.edu>
2022-07-29 14:45:09 -0400

    track packset.cpp

---
+++ lib/vegen/PackSet.cpp
@@ -0,0 +1,25 @@
+#include "PackSet.h"
+#include "vegen/Pack.h"
+#include "llvm/IR/Instruction.h"
+
+using namespace llvm;
+
+void PackSet::add(Pack *P) {
+  // Map the packed instruction back to the pack
+  for (auto *I : P->values()) {
+    if (I)
+      InstToPackMap[I] = P;
+  }
+  Packs.push_back(P);
+}
+
+Pack *PackSet::getPackForInst(Instruction *I) const {
+  return InstToPackMap.lookup(I);
+}
+
+bool PackSet::isPacked(Instruction *I) const { return InstToPackMap.count(I); }
+
+bool PackSet::isPacked(Value *V) const {
+  auto *I = dyn_cast<Instruction>(V);
+  return I && isPacked(I);
+}
