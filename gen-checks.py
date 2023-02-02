da9ce2e989f054c438ab1b5e3df993b0b5b918ed
tom <ychen306@mit.edu>
2023-02-01 22:13:15 -0500

    add basic test for vectorizing loop reduction

--- gen-checks.py
+++ gen-checks.py
@@ -7,11 +7,14 @@ import os
 llvm_root = subprocess.check_output(['llvm-config', '--src-root']).decode().strip()
 build_dir = os.path.dirname(subprocess.check_output(['which', 'pssa-opt']).decode().strip())
 
+in_filename, out_filename, *args = sys.argv[1:]
+
 with NamedTemporaryFile(suffix='.ll', mode='w', delete=False) as f:
-  f.write(f'; RUN: opt -load-pass-plugin {build_dir}/lib/libPSSA.so -passes=global-slp %s -S | FileCheck %s\n')
-  with open(sys.argv[1]) as src_f:
+  print(f'; RUN: opt -load-pass-plugin {build_dir}/lib/libPSSA.so %s -S {" ".join(args)} | FileCheck %s\n')
+  f.write(f'; RUN: opt -load-pass-plugin {build_dir}/lib/libPSSA.so %s -S {" ".join(args)} | FileCheck %s\n')
+  with open(in_filename) as src_f:
     f.write(src_f.read())
   f.flush()
 
   subprocess.call([llvm_root + '/utils/update_test_checks.py',  '--opt=opt', f.name])
-  subprocess.call(['cp', f.name, sys.argv[2]])
+  subprocess.call(['cp', f.name, out_filename])
