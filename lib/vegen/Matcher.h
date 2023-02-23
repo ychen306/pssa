8165e344eac84dbeb803c695c02e3dde41628d45
tom <ychen306@mit.edu>
2023-02-23 10:57:39 -0500

    fmt

--- lib/vegen/Matcher.h
+++ lib/vegen/Matcher.h
@@ -1,15 +1,14 @@
 #ifndef VEGEN_MATCHER_H
 #define VEGEN_MATCHER_H
 
-#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/Optional.h"
-#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/SmallVector.h"
 
 namespace llvm {
 class Value;
 }
-
 
 class Operation;
 class ReductionInfo;
@@ -26,7 +25,7 @@ class Matcher {
 public:
   Matcher(ReductionInfo &RI, LooseInstructionTable &LIT) : RI(RI), LIT(LIT) {}
 
-  // Check if we can match `V` with `Op` and fill in 
+  // Check if we can match `V` with `Op` and fill in
   llvm::ArrayRef<llvm::Value *> match(const Operation *Op, llvm::Value *V);
 };
 
